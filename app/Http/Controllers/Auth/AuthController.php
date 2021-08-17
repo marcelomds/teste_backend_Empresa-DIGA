22abacb787206b5697b1392d96fda56171556d0a
marcelomoreira <marcelo_mmds@hotmail.com>
2021-08-17 20:05:47 -0300

    completed user authentication

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string|min:6'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                $validator->errors()
+            ], 400);
+        }
+
+        $token_validity = (24 * 60);
+
+        $this->guard()->factory()->setTTL($token_validity);
+
+        if (!$token = $this->guard()->attempt($validator->validated())) {
+            return response()->json([
+                'error' => 'Não Autorizado'
+            ], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|between:2,100',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([$validator->errors()
+            ], 422);
+        }
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        return response()->json([
+            'message' => 'Usuário criado com sucesso',
+            'user' => $user
+        ]);
+    }
+
+    public function logout()
+    {
+        $this->guard()->logout();
+
+        return response()->json(['message' => 'Usuário deslogado com sucesso']);
+    }
+
+
+    public function profile()
+    {
+        return response()->json($this->guard()->user());
+    }
+
+
+    public function refresh()
+    {
+        return $this->respondWithToken($this->guard()->refresh());
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'token' => $token,
+            'token_type' => 'bearer',
+            'token_validity' => $this->guard()->factory()->getTTL() * 60
+        ]);
+    }
+
+    protected function guard()
+    {
+        return Auth::guard();
+    }
+}
